f5a39bd6d792a0c112197d936debeeb4a8c32a35
tuiza <luiza8.marlene@gmail.com>
2024-07-10 20:50:16 -0300

    feat: use components

--- app/index.tsx
+++ app/index.tsx
@@ -12,6 +12,8 @@ import {
 } from "react-native";
 import { EvilIcons } from "@expo/vector-icons";
 import { useState } from "react";
+import SearchInput from "@/components/SearchInput";
+import DailyForecast from "@/components/DailyForecast";
 
 export default function Index() {
   const [showSearch, setShowSearch] = useState(true);
@@ -34,33 +36,9 @@ export default function Index() {
         source={require("../assets/images/bg.png")}
       />
       <SafeAreaView className="flex flex-1">
-        {/* pesquisa --> fazer um componente depois */}
         <View style={{ height: "7%" }} className="mx-4 relative z-50">
-          <View
-            className="flex-row justify-end items-center rounded-full"
-            style={{
-              backgroundColor: showSearch ? theme.bgWhite(0.2) : "transparent",
-            }}
-          >
-            {showSearch && (
-              <TextInput
-                placeholder="Pesquise a cidade"
-                placeholderTextColor={"lightgray"}
-                className="pl-6 h-11 pb-1 flex-1 text-base text-white"
-              />
-            )}
-
-            <TouchableOpacity
-              style={{ backgroundColor: theme.bgWhite(0.3) }}
-              className="rounded-full  p-2 m-1"
-              onPress={() => toggleSearch()}
-            >
-              <EvilIcons name="search" size={20} color="white" />
-            </TouchableOpacity>
-          </View>
-
+          <SearchInput showSearch={showSearch} toggleSearch={toggleSearch} />
           {/* locations */}
-
           {locations.length > 0 && showSearch && (
             <View className="absolute w-full bg-gray-300 top-16 rounded-3xl ">
               <FlatList
@@ -146,83 +124,12 @@ export default function Index() {
             showsHorizontalScrollIndicator={false}
             contentContainerStyle={{ paddingHorizontal: 15, gap: 15 }}
           >
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-3"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require(`../assets/images/heavyrain.png`)}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Segunda</Text>
-              <Text className="text-white text-xl font-semibold">35&#176;</Text>
-            </View>
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-3"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require(`../assets/images/heavyrain.png`)}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Terça</Text>
-              <Text className="text-white text-xl font-semibold">35&#176;</Text>
-            </View>
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-3"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require(`../assets/images/heavyrain.png`)}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Quarta</Text>
-              <Text className="text-white text-xl font-semibold">35&#176;</Text>
-            </View>
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-3"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require(`../assets/images/heavyrain.png`)}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Quinta</Text>
-              <Text className="text-white text-xl font-semibold">35&#176;</Text>
-            </View>
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-3"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require(`../assets/images/heavyrain.png`)}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Sexta</Text>
-              <Text className="text-white text-xl font-semibold">35&#176;</Text>
-            </View>
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-3"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require(`../assets/images/heavyrain.png`)}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Sábado</Text>
-              <Text className="text-white text-xl font-semibold">35&#176;</Text>
-            </View>
-            <View
-              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-3"
-              style={{ backgroundColor: theme.bgWhite(0.15) }}
-            >
-              <Image
-                source={require(`../assets/images/heavyrain.png`)}
-                className="h-11 w-11"
-              />
-              <Text className="text-white">Domingo</Text>
-              <Text className="text-white text-xl font-semibold">35&#176;</Text>
-            </View>
+            <DailyForecast day="segunda" img={require("../assets/images/heavyrain.png")} />
+            <DailyForecast day="terça" img={require("../assets/images/heavyrain.png")} />
+            <DailyForecast day="quarta" img={require("../assets/images/heavyrain.png")} />
+            <DailyForecast day="quinta" img={require("../assets/images/heavyrain.png")} />
+            <DailyForecast day="sexta" img={require("../assets/images/heavyrain.png")} />
+          
           </ScrollView>
         </View>
       </SafeAreaView>
