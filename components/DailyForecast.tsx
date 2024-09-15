707ca882c3ff84cc1d4c7413d94e70905a730771
tuiza <luiza8.marlene@gmail.com>
2024-09-15 19:12:54 -0300

    feat: add temp

--- components/DailyForecast.tsx
+++ components/DailyForecast.tsx
@@ -3,18 +3,19 @@ import { View, Image, Text, ImageSourcePropType } from "react-native";
 
 type DailyForecastProps = {
   day: string;
-  img: ImageSourcePropType
+  img: ImageSourcePropType;
+  temp: number;
 };
 
-export default function DailyForecast({ img, day }: DailyForecastProps) {
+export default function DailyForecast({ img, day, temp }: DailyForecastProps) {
   return (
     <View
       className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-3"
       style={{ backgroundColor: theme.bgWhite(0.15) }}
     >
-      <Image source={img}className="h-11 w-11" />
+      <Image source={img} className="h-11 w-11" />
       <Text className="text-white text--uppercase">{day}</Text>
-      <Text className="text-white text-xl font-semibold">35&#176;</Text>
+      <Text className="text-white text-xl font-semibold">{temp}&#176;</Text>
     </View>
   );
 }
