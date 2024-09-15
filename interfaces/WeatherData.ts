0dc554d9ee4566b93746757ddf67ba719995e229
tuiza <luiza8.marlene@gmail.com>
2024-09-15 19:13:48 -0300

    refact: weather images

--- interfaces/WeatherData.ts
+++ interfaces/WeatherData.ts
@@ -16,8 +16,31 @@ interface Location {
 }
 
 interface ForecastDay {
-  // Definir a estrutura apropriada para os objetos internos do forecastday
+  day: {
+    maxtemp_c: number;
+    maxtemp_f: number;
+    mintemp_c: number;
+    mintemp_f: number;
+    avgtemp_c: number;
+    avgtemp_f: number;
+    maxwind_mph: number;
+    maxwind_kph: number;
+    totalprecip_mm: number;
+    totalprecip_in: number;
+    totalsnow_cm: number;
+    avgvis_km: number;
+    avgvis_miles: number;
+    avghumidity: number;
+    daily_will_it_rain: number;
+    daily_chance_of_rain: number;
+    daily_will_it_snow: number;
+    daily_chance_of_snow: number;
+    condition: WeatherCondition;
+    uv: number;
+  };
 }
+
+// Definir a estrutura apropriada para os objetos internos do forecastday
 
 interface Forecast {
   forecastday: ForecastDay[];
